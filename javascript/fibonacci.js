9b0cb5c8053d3c9513a73a11d39f4203e84788ce
github username <github email address>
2021-03-04 14:04:02 -0800

    Complete

--- javascript/fibonacci.js
+++ javascript/fibonacci.js
@@ -1,6 +1,24 @@
+//0,1,1,2,3,5,8,13,21,34,55
+// 0+1=1, 1+1=2, 2+1=3, 3+2=5, and so forth
+
+//write a function that returns the sum of previous 2 returns, starting at index 2 because it always starts with 0 and 1
+
 function fibonacci(num) {
-  // type your code here
-}
+  if (num < 2) {
+    return num;
+  }
+
+  let lastTwo = [0, 1];
+
+  for (let i = 0; i < num - 1; ++i) {
+    const sum = lastTwo[0] + lastTwo[1];
+    lastTwo = [lastTwo[1], sum];
+  }
+
+  return lastTwo[1];
+} 
+
+//0,1,1,2,3,5,8,13,21,34,55
 
 if (require.main === module) {
   // add your own tests in here
